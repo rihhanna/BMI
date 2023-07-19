f8d1418bcc4a0152cd98b190b5a8e98f33f6d061
rehhana <ashiqtalmustahiil@gmail.com>
2023-07-18 18:28:18 -0700

    answers of MBI

--- challenge.js
+++ challenge.js
@@ -1,2 +1,30 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+const ali ={
+    fullName:'ali',
+    weight:100,
+    height:1.69,
+    calculateBMI:function () {
+        this.bmi=this.weight/this.height*2;
+        return this.bmi;
+        
+    }
+
+};
+const mohamed={
+    fullName:'mohamed',
+    weight:120,
+    height:1.80,
+    calculateBMI:function(){
+        this.bmi=this.weight/this.height*2;
+        return this.bmi;                                                                                    
+    }
+}
+ali.calculateBMI();
+mohamed.calculateBMI();
+console.log(ali.mi,mohamed.bmi);
+if (ali.bmi>mohamed.bmi){
+    console.log(`${ali.fullName} 's BMI ${ali.bmi} is higher than ${mohamed.fullName}' s(${mohamed.bmi})!`);
+}else if(mohamed.BMI>ali.BMI){
+    console.log(`${mohamed.fullName}'s BMIm (${mohamed.BMI}) is higher than ${ali.fullName}'s (${ali.BMI})! `);
+}
