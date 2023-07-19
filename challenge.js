e6e42b0a5939f81025e07b52647e62fd637fc7f7
rehhana <ashiqtalmustahiil@gmail.com>
2023-07-18 18:42:45 -0700

    answers of BMI

--- challenge.js
+++ challenge.js
@@ -1,30 +1,18 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
-const ali ={
-    fullName:'ali',
-    weight:100,
-    height:1.69,
-    calculateBMI:function () {
-        this.bmi=this.weight/this.height*2;
-        return this.bmi;
-        
-    }
-
-};
-const mohamed={
-    fullName:'mohamed',
-    weight:120,
-    height:1.80,
-    calculateBMI:function(){
-        this.bmi=this.weight/this.height*2;
-        return this.bmi;                                                                                    
-    }
+function calculateBMI(weight,height){
+    const bmi =weight/(height*height);
 }
-ali.calculateBMI();
-mohamed.calculateBMI();
-console.log(ali.mi,mohamed.bmi);
-if (ali.bmi>mohamed.bmi){
-    console.log(`${ali.fullName} 's BMI ${ali.bmi} is higher than ${mohamed.fullName}' s(${mohamed.bmi})!`);
-}else if(mohamed.BMI>ali.BMI){
-    console.log(`${mohamed.fullName}'s BMIm (${mohamed.BMI}) is higher than ${ali.fullName}'s (${ali.BMI})! `);
-}
+    const mohamedWeight=70;//in kg
+    const mohamedHeight=1.70;// in meters
+    const mohamedBMI=calculateBMI(mohamedWeight,mohamedHeight)
+    const aliWeight=60; //in kg
+    const aliHeight=1.80;//in meters
+    const aliBMI=calculateBMI(aliWeight,aliHeight);
+    if (mohamedBMI>aliBMI){
+        console.log("mohamed has a higher BMI");
+    }else if (aliBMI>mohamedBMI){
+        console.log('ali has a higher MBI');
+    }else{
+        console.log("mohamed and ali have the same MBI")
+    }
